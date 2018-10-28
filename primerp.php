445cfd07111bf3a1e678c2f03dd4b7646453094b
victorveas15 <43858163+victorveas15@users.noreply.github.com>
2018-10-28 19:50:24 -0400

    Add files via upload

---
+++ primerp.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+		Plugin name: primerplugin
+		Plugin URI: http://www.primerplugin.com
+		Description: mi primer plugin 
+		Author: Victor Veas
+		Version: 1.0.0
+		Author URI: http://www.victorveas.com
+	*/
+
