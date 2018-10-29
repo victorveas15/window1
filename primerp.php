36bc7f0ef96d520af27dd08054b22dd668d25119
victorveas15 <43858163+victorveas15@users.noreply.github.com>
2018-10-28 20:05:59 -0400

    corregido y finalizado
    aquí esta Guillermo :D

--- primerp.php
+++ primerp.php
@@ -8,3 +8,12 @@
 		Author URI: http://www.victorveas.com
 	*/
 
+		add_action( 'admin_menu', 'primerplugin_add_new_menu_option' );
+			function primerplugin_add_new_menu_option() {
+				add_management_page( 'primerplugin', 'primerplugin', 'install_plugins', 'primerplugin', 'primerplugin_admin_page' );
+}
+		function primerplugin_admin_page() {
+				$text = 'Hello';
+					echo '<h1>' . $text . '</h1>';
+}
+?>
